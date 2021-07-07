5e2d009f3d5374848f955666929e8bbeea32fdb7
Celia Pennington <celiajpennington@gmail.com>
2021-07-07 14:19:47 -0500

    added activity drill down variables

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,10 @@
-//Food API https://developer.foursquare.com/docs/build-with-foursquare/categories/
-///created id userLocation in html for user input in location  
-//var userLocationInput; 
-//submitBtn = $('#submit');
-//var locationNumber = (data.response)
-function getAPI() {
+var userLocationInput; 
+var submitBtn = $('#submit');
 
-var requestURL ='https://api.foursquare.com/v2/venues/search?client_id=RTRUVKESRURB2RGWGGYSRQUJBXQDMZ2EPCM4IT33LQYPF505&client_secret=00ATNEY1RY15L0KKZ0P41ML22E4KUV5GCEW0LAPPJ5GZKG5R&v=20210706&near=austin&intent=browse&radius=10000&limit=20&categoryId=4d4b7105d754a06374d81259';
+
+function getFoodAPI() {
+
+var RequestURL ='https://api.foursquare.com/v2/venues/search?client_id=RTRUVKESRURB2RGWGGYSRQUJBXQDMZ2EPCM4IT33LQYPF505&client_secret=00ATNEY1RY15L0KKZ0P41ML22E4KUV5GCEW0LAPPJ5GZKG5R&v=20210706&near=austin&intent=browse&radius=10000&limit=20&categoryId=4d4b7105d754a06374d81259';
 fetch(requestURL)
 .then(function (response) {
     return response.json();
@@ -18,13 +17,39 @@ fetch(requestURL)
 
 }
 getAPI();
+
 ///Food drill down variables
-//var venueName = console.log(data.response.venues[0].name)
-//venueNumber
-//venueLocation = console.log(data.response.venues[0].location.formattedAddress)
-//venueFoodType = console.log(data.response.venues[8].categories[0].shortName)
+var foodVenueName = console.log(data.response.venues[0].name)
+var foodVenueLocation = console.log(data.response.venues[0].location.formattedAddress)
+var venueFoodType = console.log(data.response.venues[8].categories[0].shortName)
+
+
+//This is the event request URL with appropriate category ID "Arts & Entertainment 4d4b7104d754a06370d81259"
+
+function getEventAPI(){
+var requestURL ='https://api.foursquare.com/v2/venues/search?client_id=RTRUVKESRURB2RGWGGYSRQUJBXQDMZ2EPCM4IT33LQYPF505&client_secret=00ATNEY1RY15L0KKZ0P41ML22E4KUV5GCEW0LAPPJ5GZKG5R&v=20210706&near=austin&intent=browse&radius=10000&limit=20&categoryId=4d4b7104d754a06370d81259';
+fetch(requestURL)
+.then(function (response) {
+    return response.json();
+    })
+    .then(function (data) {
+        
+      console.log(data.response.venues)
+    
+    })
+
+}
+getEventAPI();
+
 
 //Event drilldown variables
+var activityVenueName = console.log(data.response.venues[0].name);
+var activityVenueLocation = console.log(data.response.venues[0].location.formattedAddress);
+
+
+
+
+
 
 userLocationInput = $('#userLocation').val(); 
 
@@ -43,6 +68,12 @@ userLocationInput = $('#userLocation').val();
         
         //})
 //}
+
+
+
+
+
+
 
 
 
